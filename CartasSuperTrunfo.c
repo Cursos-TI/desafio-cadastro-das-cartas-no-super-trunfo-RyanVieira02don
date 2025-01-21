db9624dd6d128c848b970fa01c340ed4d5c83ce0
RyanVieira02don <donryan493@gmail.com>
2025-01-21 02:41:28 +0000

    Criei o Printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,6 +17,32 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    
+    char codigo[4] = "A01";
+    char nome[50] = "Sao Paulo";
+    char populacao[14] = "11.451.999MI";
+    char area[11] = "1.521,20KM";
+    char PIB[8] = "2,719BI"; 
 
+   printf("codigo: %s\n", codigo);
+   printf("nome: %s\n", nome);
+   printf("populacao: %s\n", populacao);
+   printf("area: %s\n", area);
+   printf("PIB: %s\n", PIB);
+
+   printf("Digite o codigo da cidade:\n");
+   scanf("%s", &codigo);
+
+   printf("Digite o nome da cidade:\n");
+   scanf("%s", &nome); 
+
+   printf("Digite a populacao:\n");
+   scanf("%s", &populacao);
+
+   printf("Digite a area da cidade:\n");
+   scanf("%s", &area);
+
+   printf("Digite o PIB da cidade:\n");
+   scanf("%s", &PIB);
     return 0;
-}
+}   
